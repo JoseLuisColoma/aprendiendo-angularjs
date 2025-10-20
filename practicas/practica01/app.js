5e240053ba884c9c4af343c670b1e3924fae69cf
Jose Luis Coloma <colomatormojl@gmail.com>
2025-10-20 21:48:22 +0200

    Actualiza ficheros practica01

--- practicas/practica01/app.js
+++ practicas/practica01/app.js
@@ -1,27 +1,26 @@
 function ServicioMundos() {
-    return {
-        obtenerMundos: function () {
-            return ['Tierra', 'Mercurio', 'Marte', 'Venus', 'Júpiter', 'Saturno', 'Urano', 'Neptuno', 'Plutón'];
-        }
-    };
+  return {
+    obtenerMundos: function () {
+      return ['Tierra', 'Mercurio', 'Marte', 'Venus', 'Júpiter', 'Saturno', 'Urano', 'Neptuno', 'Plutón'];
+    }
+  };
 }
 
 function ControladorDemo(ServicioMundos) {
-    var vm = this;
-    vm.mensajes = ServicioMundos.obtenerMundos().map(function (mundo) {
-        return '¡Hola ' + mundo + '!';
-    });
+  this.mensajes = ServicioMundos.obtenerMundos().map(function (planeta) {
+    return 'Saludos desde el planeta ' + planeta + '!';
+  });
 }
 
 function alIniciar($rootScope, $window) {
-    $window.alert("¡Hola usuario! Cargando mundos...");
-    $rootScope.yaInicio = true;
+  $window.alert("¡Hola usuario! Cargando mundos...");
+  $rootScope.yaInicio = true;
 }
 
 angular.module('miAppDemo', [])
-    .service('ServicioMundos', [ServicioMundos])
-    .controller('ControladorDemo', ["ServicioMundos", ControladorDemo])
-    .config(function () {
-        console.log("Configurando la aplicación...");
-    })
-    .run(["$rootScope", "$window", alIniciar]);
+  .service('ServicioMundos', [ServicioMundos])
+  .controller('ControladorDemo', ["ServicioMundos", ControladorDemo])
+  .config(function () {
+    console.log("Configurando la aplicación...");
+  })
+  .run(["$rootScope", "$window", alIniciar]);
